55cff8d6351f270788829b8350edb6328fcc0326
dmakger <dmakger@yandex.ru>
2024-03-30 19:09:18 +0300

    Create category

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+#  КАТЕГОРИЯ
+from category.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id']
+
+
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# КАТЕГОРИЯ.
+class Category(models.Model):
+    title = models.CharField('Название', max_length=128)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.title
