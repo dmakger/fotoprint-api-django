c4af017d5362cd8285ed8d26f5848371f773c5f3
dmakger <dmakger@yandex.ru>
2024-03-30 19:22:35 +0300

    Create product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from product.models import Product, ProductToCharacteristicItem
 
-# Register your models here.
+
+#  ПРОДУКТ
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'image', 'execution_time', 'id']
+
+
+# ПРОДУКТ к ЭЛЕМЕНТУ ХАРАКТЕРИСТИК
+class ProductToCharacteristicItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'characteristic_item', 'group_characteristic', 'price', 'number', 'id']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductToCharacteristicItem, ProductToCharacteristicItemAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from category.models import Category
+from characteristic.models import CharacteristicItem, GroupCharacteristic
+
+
+# ПРОДУКТ
+class Product(models.Model):
+    title = models.CharField('Название', max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    image = models.ImageField('Изображение', upload_to='product/images/', null=True, default=None, blank=True)
+    execution_time = models.CharField('Количество рабочих дней', max_length=128, null=True, default=None, blank=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.title
+
+
+# ПРОДУКТ к ЭЛЕМЕНТУ ХАРАКТЕРИСТИК
+class ProductToCharacteristicItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Проект')
+    characteristic_item = models.ForeignKey(CharacteristicItem, on_delete=models.CASCADE,
+                                            verbose_name='Элемент характеристик')
+    group_characteristic = models.ForeignKey(GroupCharacteristic, on_delete=models.CASCADE, verbose_name='Проект')
+    price = models.FloatField('Стоимость', default=0)
+    number = models.IntegerField('Порядковый номер', default=1)
+
+    class Meta:
+        verbose_name = "Продукт к Характеристикам"
+        verbose_name_plural = "Продукты к Характеристикам"
+
+    def __str__(self):
+        return f"{self.product} {self.characteristic_item}"
