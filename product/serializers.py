ba2631617fa851b92de1e28d3a123b1382bc931c
dmakger <dmakger@yandex.ru>
2024-07-26 19:19:04 +0300

    Add serializers forms

--- product/serializers.py
+++ product/serializers.py
@@ -1,8 +1,9 @@
+from django.db.models import QuerySet
 from rest_framework import serializers
 
 from category.serializers import CategorySerializer
 from characteristic.serializers import CombinationSerializer, ExecutionTimeSerializer, CharacteristicSerializer
-from product.models import Product, ProductCharacteristicCombination
+from product.models import Product, ProductCharacteristicCombination, ProductFormCombination, ProductForm
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -46,3 +47,40 @@ class ProductCharacteristicAllCombinationsSerializer(serializers.ModelSerializer
     def get_characteristics(self, instance: ProductCharacteristicCombination):
         characteristics = instance.combination.get_full_children()
         return CharacteristicSerializer(characteristics, many=True).data
+
+
+class ProductFormAllCombinationsSerializer(serializers.ModelSerializer):
+    """
+    Сериализация `ProductFormCombination`.
+    Добавляет `characteristics` список всех характеристик в данной комбинации
+    """
+    # product = ProductSerializer()
+    characteristics = serializers.SerializerMethodField()
+    execution_time = ExecutionTimeSerializer()
+
+    class Meta:
+        model = ProductFormCombination
+        # fields = ['id', 'product_form', 'characteristics', 'execution_time']
+        fields = ['id', 'characteristics', 'execution_time']
+
+    def get_characteristics(self, instance: ProductFormCombination):
+        characteristics = instance.combination.get_full_children()
+        return CharacteristicSerializer(characteristics, many=True).data
+
+
+class ProductAllFormSerializer(serializers.ModelSerializer):
+    """
+    Сериализация `ProductFormCombination`.
+    Добавляет `characteristics` список всех характеристик в данной комбинации
+    """
+    # product = ProductSerializer()
+    combinations = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ProductForm
+        # fields = ['id', 'product_form', 'characteristics', 'execution_time']
+        fields = ['id', 'title', 'combinations']
+
+    def get_combinations(self, instance: ProductForm):
+        forms: QuerySet[ProductFormCombination] = ProductFormCombination.objects.filter(product_form=instance)
+        return ProductFormAllCombinationsSerializer(forms, many=True).data
--- product/views/detail/detail_serializers.py
+++ product/views/detail/detail_serializers.py
@@ -3,9 +3,9 @@ from rest_framework import serializers
 
 from category.serializers import CategorySerializer
 from characteristic.serializers import CombinationSerializer, CharacteristicSerializer
-from product.models import Product, ProductCharacteristicCombination
+from product.models import Product, ProductCharacteristicCombination, ProductForm
 from product.serializers import ProductCharacteristicCombinationSerializer, \
-    ProductCharacteristicAllCombinationsSerializer
+    ProductCharacteristicAllCombinationsSerializer, ProductFormAllCombinationsSerializer, ProductAllFormSerializer
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
@@ -14,6 +14,7 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     """
     category = CategorySerializer()
     combinations = serializers.SerializerMethodField()
+    forms = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -22,4 +23,8 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     def get_combinations(self, instance: Product):
         qs: QuerySet[ProductCharacteristicCombination] = ProductCharacteristicCombination.objects.filter(product=instance)
         return ProductCharacteristicAllCombinationsSerializer(qs, many=True).data
+
+    def get_forms(self, instance: Product):
+        qs: QuerySet[ProductForm] = ProductForm.objects.filter(product=instance)
+        return ProductAllFormSerializer(qs, many=True).data
 
